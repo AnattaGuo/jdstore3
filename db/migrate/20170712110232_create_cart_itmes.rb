a7471a8ca2df8975b64e48d753a2b8a466dcdf37
AnattaGuo <mengjunkwok@gmail.com>
2017-07-12 19:07:05 +0800

    implement add_product_to_cart function

---
+++ db/migrate/20170712110232_create_cart_itmes.rb
@@ -0,0 +1,10 @@
+class CreateCartItmes < ActiveRecord::Migration[5.0]
+  def change
+    create_table :cart_itmes do |t|
+       t.integer :cart_id
+       t.integer :product_id
+       t.integer :quantity, default: 1
+      t.timestamps
+    end
+  end
+end
